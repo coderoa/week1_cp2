511853766cb3d5cf8fdb867ff3544667fe74612f
Ozodbek <e@e.com>
2026-01-30 14:54:58 +0500

    test

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+import java.math;
+void main(){
+
+
+    //Problem 3
+    float height = 7.9f;
+    float width = 4.5f;
+    float area = height*width;
+    float perimeter = 2*(height+width);
+    System.out.println("The area="+area);
+    System.out.println("Perimeter="+perimeter);
+
+
+    //Problem 4
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the radius and length of cylinder: ");
+    double radius = input.nextDouble();
+    double length = input.nextDouble();
+
+
+}
