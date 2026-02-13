fb3613861036cdc93212b3dcd1593f216e864c13
coderoa <johnsonliberty73@gmail.com>
2026-02-13 16:37:05 +0500

    Add files via upload
    This file is for week3. COMPUTER PROGRAMMING 2 CLASS

--- Main.java
+++ Main.java
@@ -1,66 +1,112 @@
-
-//Problem 1
-//void main(String[] args){
-//    int sum=0;
-//    for(String number:args){
-//        sum+=Integer.parseInt(number);
-//    }
-//    System.out.println(sum);
-
-
-
-//import java.util.Random;
-//Problem 2
+////Problem 1
+//import java.util.Scanner;
 //void main(){
-//    Random rand = new Random();
-//    int radius = rand.nextInt(1,10);
-//    double circumference = 2*Math.PI*radius;
-//    double area = Math.PI*Math.pow(radius, 2);
-//    System.out.printf("Circumference: %.2f%n", circumference);
-//    System.out.printf("Area: %.2f%n",area);
+//    Scanner input = new Scanner(System.in);
+//    String text;
+//    text = input.nextLine();
+//    System.out.println(reverse(text));
+//}
+//
+//public static String reverse(String s){
+//    String reversed="";
+//    for(int i=s.length()-1; i>=0; i--){
+//        reversed += s.charAt(i);
+//    }
+//    return reversed;
 //}
 
-//Problem 3
-//import java.util.Scanner;
-//void main() {
+
+////Problem 2
+//void main(){
 //    Scanner input = new Scanner(System.in);
-//    int score = input.nextInt();
 //
-//    if (score < 0 || score > 100) {
-//        System.out.println("Invalid score");
-//    } else if (score >= 90) {
-//        System.out.println("Grade: A");
-//    } else if (score >= 80) {
-//        System.out.println("Grade: B");
-//    } else if (score >= 70) {
-//        System.out.println("Grade: C");
-//    } else if (score >= 60) {
-//        System.out.println("Grade: D");
-//    } else {
-//        System.out.println("Grade: F");
+//    String text;
+//    char character;
+//
+//    System.out.print("Enter a text a: ");
+//    text = input.nextLine();
+//
+//    System.out.print("Enter a character: ");
+//    character = input.next().charAt(0);
+//
+//    System.out.println(count(text, character));
+//
+//}
+//
+//public static int count(String text, char character){
+//    int counter=0;
+//    for (int i=0; i<text.length();i++){
+//        if(text.charAt(i) == character){
+//            counter++;
+//        }
 //    }
+//
+//    return counter;
+//}
+
+
+
+////Problem 3
+//
+//void main(String[] args){
+//    if (args.length==0){
+//        System.out.println("you did not enter any text!");
+//        return;
+//    }
+//
+//    int uppercasecounter =0 ;
+//    int lowercasecounter = 0;
+//    String text = String.join(" ", args);
+//    for (int i=0; i<text.length(); i++){
+//        char ch = text.charAt(i);
+//        if (Character.isUpperCase(ch)){
+//            uppercasecounter++;
+//        }
+//        else if(Character.isLowerCase(ch)){
+//            lowercasecounter++;
+//        }
+//    }
+//    System.out.println("in your text "+text+". There are ");
+//    System.out.println("Uppercase letters: "+uppercasecounter);
+//    System.out.println("Lowercase letters: "+lowercasecounter);
+//
 //}
 
 
 //Problem 4
-//
-//import java.util.Scanner;
-//void main(){
-//    Scanner input = new Scanner(System.in);
-//    System.out.println("Hello, \nEnter your weight in KG: ");
-//    float weight = input.nextFloat();
-//    System.out.println("Enter you height in meter");
-//    float height = input.nextFloat();
-//    float BMI = (float) (weight/Math.pow(height, 2));
-//    System.out.println("Your BMI is: "+BMI);
-//    if (BMI>30){
-//        System.out.println("Obese");
-//    }else if(BMI>25 && BMI<29.9){
-//        System.out.println("Overweight");
-//    }else if(BMI>18.5 && BMI<24.9){
-//        System.out.println("Normal weight");
-//    }else{
-//        System.out.println("Underweight");
-//    }
-//}
+void main(){
+
+    Scanner input = new Scanner(System.in);
+
+
+}
+
+
+public static int indexOfSmallestElement(double[] array){
+
+    int index = 0;
+    double min = array[0];
+    for(int i; i<array.length;i++){
+        if(array[i]<min){
+            min=array[i];
+            index=i;
+        }
+    }
+    return index;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
